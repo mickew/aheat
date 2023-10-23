d66172dbb232dd248887af60319cfe355f7c19c3
Mikael Wågberg <mikael_wagberg@trimble.com>
2023-10-23 22:17:22 +0200

    Server base code

--- AHeat.Web.API/Program.cs
+++ AHeat.Web.API/Program.cs
@@ -1,5 +1,11 @@
 
+using System.IdentityModel.Tokens.Jwt;
+using AHeat.Web.API.Authorization;
 using AHeat.Web.API.Data;
+using AHeat.Web.API.Models;
+using AHeat.Web.Shared.Authorization;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.EntityFrameworkCore;
 
 namespace AHeat.Web.API;
@@ -14,11 +20,38 @@ public class Program
         var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
         builder.Services.AddDbContext<ApplicationDbContext>(options =>
             options.UseSqlite(connectionString));
+        builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-        builder.Services.AddControllers();
+        builder.Services
+            .AddDefaultIdentity<User>(options => options.SignIn.RequireConfirmedAccount = false)
+            .AddRoles<Role>()
+            .AddEntityFrameworkStores<ApplicationDbContext>()
+            .AddClaimsPrincipalFactory<ApplicationUserClaimsPrincipalFactory>();
+
+        builder.Services.AddIdentityServer()
+            .AddApiAuthorization<User, ApplicationDbContext>(options =>
+            {
+                options.IdentityResources["openid"].UserClaims.Add("role");
+                options.ApiResources.Single().UserClaims.Add("role");
+                options.IdentityResources["openid"].UserClaims.Add("permissions");
+                options.ApiResources.Single().UserClaims.Add("permissions");
+            });
+
+        JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Remove("role");
+
+        builder.Services.AddAuthentication()
+            .AddIdentityServerJwt();
+
+        builder.Services.AddControllersWithViews();
+        builder.Services.AddRazorPages();
+
+
         // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
         builder.Services.AddEndpointsApiExplorer();
         builder.Services.AddSwaggerGen();
+
+        builder.Services.AddSingleton<IAuthorizationHandler, PermissionAuthorizationHandler>();
+        builder.Services.AddSingleton<IAuthorizationPolicyProvider, FlexibleAuthorizationPolicyProvider>();
 
         var app = builder.Build();
 
@@ -28,13 +61,30 @@ public class Program
             app.UseSwagger();
             app.UseSwaggerUI();
         }
+        else
+        {
+            app.UseExceptionHandler("/Error");
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
+        }
 
         app.UseHttpsRedirection();
 
+        app.UseBlazorFrameworkFiles();
+        app.UseStaticFiles();
+
+        app.UseRouting();
+
+        app.UseAuthorization();
+
+        app.UseIdentityServer();
+        app.UseAuthentication();
         app.UseAuthorization();
 
 
+        app.MapRazorPages();
         app.MapControllers();
+        app.MapFallbackToFile("index.html");
 
         app.Run();
     }
